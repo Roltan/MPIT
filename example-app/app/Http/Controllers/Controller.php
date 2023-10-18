a819dca7c4862cc04e812f66b67f6e193d425306
Roltan <KhalikoR@yandex.ru>
2023-10-18 15:50:54 +0500

    не тестируемый поиск

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -20,30 +20,46 @@ class Controller extends BaseController
         $radius = $request->input('radius');
         $like = $request->input('like');
 
-        $parkBD = DB::select('SELECT * FROM park ORDER BY reiting DESC');
-        // и так импоритировать все таблицы с местами
-        dd($parkBD);
+        if(DB::table('hotel')->where('name',$hotel)->exists()){
+            $dolgA = DB::select("SELECT dolg FROM hotel WHERE name=$hotel");
+            $shirA = DB::select("SELECT shir FROM hotel WHERE name=$hotel");
 
-        $dolgA = DB::select("SELECT dolg FROM hotel WHERE name=$hotel");
-        $shirA = DB::select("SELECT shir FROM hotel WHERE name=$hotel");
+            $id = 1;
+            switch($like){
+                case "park":
+                    // проверить есть ли в окружности это место
+                    while (true) {
+                        if(DB::table('park')->where('id',$id)->exists()){
+                            $dolg = DB::select("SELECT dolg FROM park WHERE id=$id");
+                            $shir = DB::select("SELECT shir FROM park WHERE id=$id");
 
-        $id = 1;
-        switch($like){
-            case "park":
-                // проверить есть ли в окружности это место
-                while (true) {
-                    if(DB::table('park')->where('id',$id)->exists()){
-                        
+                            $s = ($shir[0] - $shirA[0])^2 + ($dolg[0] - $dolgA[0])^2;
+                            $s = sqrt($s);
+                            if($s > $radius){
+                                // неотображать
+                                dd('не нашли');
+                            }
+                            else{
+                                // отобразить
+                                $name = DB::select("SELECT name FROM park WHERE id=$id");
+                                $itogList[] = $name[0];
+                            }
+                        }
+                        else{
+                            break;
+                        }
+                        $id++;
                     }
-                    else{
-                        break;
-                    }
-                    $id++;
-                }
-                break;
-            default:
-                // у нас ещё нет такой котигории
-                break;
+                    break;
+
+                // тут добовлять case для каждой котегории
+
+                default:
+                    // у нас ещё нет такой котигории
+                    dd("у нас ещё нет такой катигории");
+                    break;
+            }
+            dd($itogList);
         }
     }
 }
